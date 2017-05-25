72fe440ab93d8c2d8025daa5563b7ccc22b06953
glennsl <glenn@slotte.net>
2017-05-25 20:21:03 +0200

    add bs-glob dependency, remove local glob module

--- src/glob.mli
+++
@@ -1,4 +0,0 @@
-type error
-
-external glob : string -> (error Js.null -> string array -> unit) -> unit = "" [@@bs.module]
-external sync : string -> string array = "" [@@bs.val] [@@bs.module "glob"]
